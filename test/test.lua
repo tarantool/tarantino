3f4cee0a0261c42b5db0a22940f7680d1eaad874
Andrey Drozdov <andrey.drozdov@msk-wire-andrey_drozdov-andrey.drozdov.mail.msk>
2016-11-01 22:27:45 +0300

    Nginx supervisor updated, test moved to docker

--- test/test.lua
+++ test/test.lua
@@ -3,8 +3,8 @@ local http = require('http.client')
 local json = require('json')
 local fiber = require('fiber')
 local tap = require('tap')
-local debug = false
-local base = 'http://sh2.tarantool.org'
+local debug = true
+local base = 'http://172.17.0.2'
 function request(r_type, uri, body, headers)
     local header = {headers={}}
     if headers ~= nil then
@@ -26,38 +26,38 @@ test:plan(1)
 
 test:test("Simple test", function(test)
     test:plan(11)
-    local resp = request('delete', '/test/1/user/1')
+    local resp = request('delete', '/api/1/user/1')
     test:is(resp.status.code, 200, "Delete first")
-    resp = request('delete', '/test/1/user/2')
+    resp = request('delete', '/api/1/user/2')
     test:is(resp.status.code, 200, "Delete second")
 
-    resp = request('put', '/test/1/user/1',
+    resp = request('put', '/api/1/user/1',
         '{"params":[{"uid":1,"first_name":"andrey","last_name":"drozdov"}]}'
     )
     test:is(resp.status.code, 200, "Put first")
-    resp = request('put', '/test/1/user/2',
+    resp = request('put', '/api/1/user/2',
         '{"params":[{"uid":2,"first_name":"Andrey2","last_name":"Drozdov"}]}'
     )
     test:is(resp.status.code, 200, "Put second")
 
-    resp = request('get', '/test/1/user/1')
+    resp = request('get', '/api/1/user/1')
     test:is(resp.status.code, 200, "Check first")
-    resp = request('get', '/test/1/user/2')
+    resp = request('get', '/api/1/user/2')
     test:is(resp.status.code, 200, "Check second")
-    resp = request('get', '/test/1/user')
+    resp = request('get', '/api/1/user')
     test:is(#resp.data, 2, "Check list query")
-    resp = request('get', '/test/1/user?limit=1')
+    resp = request('get', '/api/1/user?limit=1')
     test:is(#resp.data, 1, "Check limited list query")
 
-    resp = request('post', '/test/1/user/2',
+    resp = request('post', '/api/1/user/2',
         '{"params":[{"last_name":"test update"}]}'
     )
     test:is(resp.status.code, 200, "Exec update")
 
-    resp = request('get', '/test/1/user/2')
+    resp = request('get', '/api/1/user/2')
     test:is(resp.data.last_name, 'test update', "Check update request")
 
-    resp = request('post', '/test/1/user/2',
+    resp = request('post', '/api/1/user/2',
         '{"params":[{"last_name":123}]}'
     )
     test:is(resp.status.code, 400, "Update with bad avro")
